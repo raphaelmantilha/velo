df38bff46afff6f6be636f79f3b9ca40407cb9cc
Raphael Mantilha <raphael.mantilha@gmail.com>
2026-02-07 17:27:23 -0300

    resolução do desafio 'Sobreviva sem Data Test IDs'

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -7,16 +7,24 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
   //Arrange
   await page.goto('http://localhost:5173/');
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint'); 
+  
   await page.getByRole('link', { name: 'Consultar Pedido' }).click();   //a[text()='Consultar Pedido']
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
 
   // Act 
-  await page.getByTestId('search-order-id').fill('VLO-1LKL3P');
-  await page.getByTestId('search-order-button').click();
+  //await page.getByTestId('search-order-id').fill('VLO-1LKL3P');
+  //await page.getByLabel('Número do Pedido').fill('VLO-1LKL3P');
+  //await page.getByPlaceholder('Ex: VLO-ABC123').fill('VLO-1LKL3P');
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-1LKL3P');
+  
+  //await page.getByTestId('search-order-button').click();
+  //await page.locator('//button[text()="Buscar Pedido"]').click();
+  await page.getByRole('button', { name: 'Buscar Pedido' }).click();
 
   // Assert
-  await expect(page.getByTestId('order-result-id')).toBeVisible();
-  await expect(page.getByTestId('order-result-id')).toContainText('VLO-1LKL3P');
-  await expect(page.getByTestId('order-result-status')).toBeVisible();
-  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
+  await expect(page.locator('//p[text()="Pedido"]/following-sibling::p')).toBeVisible({timeout: 10_000});
+  await expect(page.locator('//p[text()="Pedido"]/following-sibling::p')).toContainText('VLO-1LKL3P');
+
+  await expect(page.locator('//div[text()="APROVADO"]')).toBeVisible();
+  await expect(page.locator('//div[text()="APROVADO"]')).toContainText('APROVADO');
 });
